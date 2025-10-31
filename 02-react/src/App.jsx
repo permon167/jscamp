4e8ae67f47bf4cf293e230270e8d20c594edf20c
permon167 <permon1@posgrado.upv.es>
2025-10-31 11:44:05 +0100

    Avanzo con los hooks y los formularios de buscar y filtros con React

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
@@ -11,9 +11,30 @@ import jobsData from "./data.json";
 const RESULTS_PER_PAGE = 4;
 
 function App() {
+  const [filters, setFilters] = useState({
+    technology: "",
+    location: "",
+    experienceLevel: "",
+  });
+  const [textToFilter, setTextToFilter] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE);
-  const pageResults = jobsData.slice(
+
+  const jobsFilteredByFilters = jobsData.filter((job) => {
+    return (
+      filters.technology === "" || job.data.tecnologia === filters.technology
+    );
+  });
+
+  const jobsWithTextFilter =
+    textToFilter === ""
+      ? jobsFilteredByFilters
+      : jobsFilteredByFilters.filter((job) => {
+          return job.titulo.toLowerCase().includes(textToFilter.toLowerCase());
+        });
+
+  const totalPages = Math.ceil(jobsWithTextFilter.length / RESULTS_PER_PAGE);
+
+  const pageResults = jobsWithTextFilter.slice(
     (currentPage - 1) * RESULTS_PER_PAGE, //Pagina 1 -> 0
     currentPage * RESULTS_PER_PAGE //Pagina 1 -> 5
   );
@@ -22,12 +43,27 @@ function App() {
     console.log("Cambiando a la pagina", page);
     setCurrentPage(page);
   };
+
+  const handleSearch = (filters) => {
+    setFilters(filters);
+    setCurrentPage(1);
+  };
+
+  const handleTextFilter = (newTextToFilter) => {
+    setTextToFilter(newTextToFilter);
+    setCurrentPage(1);
+  };
+
+  /* HTML */
   return (
     <>
       <Header />
 
       <main>
-        <SearchFormSection />
+        <SearchFormSection
+          onSearch={handleSearch}
+          onTextFilter={handleTextFilter}
+        />
 
         <section>
           <JobListings jobs={pageResults} />
--- 02-react/src/components/SearchFormSection.jsx
+++ 02-react/src/components/SearchFormSection.jsx
@@ -1,13 +1,35 @@
 import { useId } from "react";
 
-export function SearchFormSection() {
+export function SearchFormSection({ onTextFilter, onSearch }) {
+  const idText = useId();
+  const idTechnology = useId();
+  const idLocation = useId();
+  const idExperience = useId();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const filters = {
+      search: formData.get(idText),
+      technology: formData.get(idTechnology),
+      location: formData.get(idLocation),
+      experienceLevel: formData.get(idExperience),
+    };
+    onSearch(filters);
+  };
+
+  const handleTextChange = (event) => {
+    const text = event.target.value;
+    onTextFilter(text);
+  };
+
   return (
-    <section class="jobs-search">
+    <section className="jobs-search">
       <h1>Encuentra tu próximo trabajo</h1>
       <p>Explora miles de oportunidades en el sector tecnologico</p>
 
-      <form id="empleos-search-form" role="search">
-        <div class="search-bar">
+      <form onSubmit={handleSubmit} id="empleos-search-form" role="search">
+        <div className="search-bar">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -15,55 +37,56 @@ export function SearchFormSection() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="1"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-search"
+            strokeWidth="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-search"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
             <path d="M21 21l-6 -6" />
           </svg>
+
           <input
-            name="search"
+            name={idText}
             id="empleos-search-input"
-            required
             type="text"
             placeholder="Buscar trabajos, empresas o habilidades"
+            onChange={handleTextChange}
           />
           <button type="submit">Buscar</button>
         </div>
+
+        {/* Apartado Filtros */}
+        <div className="search-filters">
+          <select name={idTechnology} id="filter-technology">
+            <option value="">Tecnología</option>
+            <option value="javascript">JavaScript</option>
+            <option value="python">Python</option>
+            <option value="react">React</option>
+            <option value="node">Node.js</option>
+            <option value="sql">SQL</option>
+            <option value="mobile">Mobile</option>
+          </select>
+
+          <select name={idLocation} id="filter-location">
+            <option value="">Ubicación</option>
+            <option value="remoto">Remoto</option>
+            <option value="cdmx">Ciudad de México</option>
+            <option value="guadalajara">Guadalajara</option>
+            <option value="monterrey">Monterrey</option>
+            <option value="barcelona">Barcelona</option>
+          </select>
+
+          <select name={idExperience} id="filter-experience-level">
+            <option value="">Nivel de experiencia</option>
+            <option value="junior">Junior</option>
+            <option value="mid-level">Mid-level</option>
+            <option value="senior">Senior</option>
+            <option value="lead">Lead</option>
+          </select>
+        </div>
       </form>
-
-      {/* Filtros */}
-      <div class="search-filters">
-        <select name="technology" id="filter-technology">
-          <option value="">Tecnología</option>
-          <option value="javascript">JavaScript</option>
-          <option value="python">Python</option>
-          <option value="react">React</option>
-          <option value="node">Node.js</option>
-          <option value="sql">SQL</option>
-          <option value="mobile">Mobile</option>
-        </select>
-
-        <select name="location" id="filter-location">
-          <option value="">Ubicación</option>
-          <option value="remoto">Remoto</option>
-          <option value="cdmx">Ciudad de México</option>
-          <option value="guadalajara">Guadalajara</option>
-          <option value="monterrey">Monterrey</option>
-          <option value="barcelona">Barcelona</option>
-        </select>
-
-        <select name="experience-level" id="filter-experience-level">
-          <option value="">Nivel de experiencia</option>
-          <option value="junior">Junior</option>
-          <option value="mid-level">Mid-level</option>
-          <option value="senior">Senior</option>
-          <option value="lead">Lead</option>
-        </select>
-      </div>
     </section>
   );
 }
