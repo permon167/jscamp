29746120c7a25900dbcb71ea4602b0f6fe0514a5
Pablo <pablopermon16@gmail.com>
2026-01-15 13:58:13 +0000

    creacion y manejo de las rutas,: muestra y creacion de usuarios

--- 04-node/manage-files.js
+++ 04-node/manage-files.js
@@ -1,10 +1,28 @@
-import {readFile, writeFile} from "node:fs/promises"
-import {join, basename, extname} from "node:path"
+import {mkdir, readFile, writeFile} from "node:fs/promises"
+import {join, basename, extname} from "node:path" // el path para manejar rutas en Windows
 
-const content = await readFile("./archivo.txt", "utf-8")
-console.log(content)
+let content = ""
+//permisos en terminal con --allow fs-read --allow fs-write=...
+if(process.permissions.has("fs:read", "archivo.txt")){
+    content = await readFile("archivo.txt", "utf-8")
+    console.log(content)
+}else{
+    console.log("No tienes permiso para leer el archivo.")
+}
 
-const uppercaseContent = content.toUpperCase()
+if (process.permissions.has("fs:write", "output/files/documents")) {
+    //join para crear rutas de forma correcta para Windows, Linux, Mac
+    const outputDir = join("output", "files", "documents")
+    await mkdir(outputDir, { recursive: true })
 
-await writeFile("./archivo-mayusculas.txt", uppercaseContent)
-console.log("Archivo creado con contenido en mayúsculas.")
+    const uppercaseContent = content.toUpperCase()
+    const outputFilePath = join(outputDir, "archivo-mayusculas.txt")
+
+    //basename y extname para manejar nombres y extensiones de archivos
+    console.log(basename(outputFilePath, extname(outputFilePath))) // archivo-mayusculas
+
+    await writeFile(outputFilePath, uppercaseContent)
+    console.log("Archivo creado con contenido en mayúsculas.")
+} else {
+    console.log("No tienes permiso para escribir en el directorio.")
+}
--- 04-node/server.js
+++ 04-node/server.js
@@ -1,13 +1,68 @@
 import { createServer } from "node:http";
+import {json} from "node:stream/consumers";
+import  {randomUUID} from "node:crypto";
+import path from "node:path";
 
+process.loadEnvFile(); // Carga variables de entorno desde un archivo .env si existe
+
+//environment variable PORT o 3000
 const port = process.env.port ?? 3000;
 
-const server = createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end('Hello, this is a simple Node.js server!\n');
+// Función para enviar respuestas JSON
+function sendJsonResponse(res, statusCode, data) {
+    res.statusCode = statusCode;
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    return res.end(JSON.stringify(data));
+}
+
+const users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+];
+
+//funcion debe ser asincrona para usar el await del post crear usuarios
+const server = createServer(async (req, res) => {
+    const { method, url } = req;
+
+    const [pathName, queryString] = url.split('?');
+
+    const searchParams = new URLSearchParams(queryString);
+
+    if (method === "GET") {
+        if (pathName === '/users') {
+            const limit = Number(searchParams.get('limit')) || users.length;
+            const offset = Number(searchParams.get('offset')) || 0;
+            const paginatedUsers = users.slice(offset, offset + limit);
+            //cabecera de json con application/json y utf-8
+            return sendJsonResponse(res, 200, paginatedUsers);
+        }
+
+        // Endpoint para verificar el estado del servidor
+        if (pathName === '/health') {
+            return sendJsonResponse(res, 200, { status: 'OK', uptime: process.uptime() });
+        }
+
+    }
+
+    if (method === "POST") {
+        if (pathName === '/users') {
+            //await es asincrono
+            const body = await json(req);
+            if(!body || !body.name){
+                return sendJsonResponse(res, 400, { message: 'Name is required' });
+            }
+            const newUser = { name: body.name, id: randomUUID() };
+            users.push(newUser);
+
+            return sendJsonResponse(res, 201, { message: 'User created successfully' });
+        }
+    }
+
+    return sendJsonResponse(res, 404, { message: 'Not Found' });
 });
 
 server.listen(port, () => {
+    //adress nos obtiene el primer puerto disponible
     const address = server.address();
     console.log(`Server is running at http://localhost:${address.port}/`);
 });
